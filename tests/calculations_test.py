7c4c2da7d7681f866f1e2d8e5e80bfe2e672d492
Ben Watson <bw1petra@gmail.com>
2025-10-20 18:54:51 +0000

    Uncomment test_get_nth_fibonacci_ten in calculations_test.py

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -58,13 +58,14 @@ def test_get_nth_fibonacci_one():
     assert result == 1
 
 
-# def test_get_nth_fibonacci_ten():
-#     """Test with n=10."""
-#     # Arrange
-#     n = 10
 
-#     # Act
-#     result = get_nth_fibonacci(n)
+def test_get_nth_fibonacci_ten():
+    """Test with n=10."""
+    # Arrange
+    n = 10
 
-#     # Assert
-#     assert result == 89
+    # Act
+    result = get_nth_fibonacci(n)
+
+    # Assert
+    assert result == 89
